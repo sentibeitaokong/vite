723fbcb4cb0faff0570605f0370d2c729c1396d2
sentibeitaokong <1120996904@qq.com>
2026-06-03 11:28:21 +0800

    feat: deploy vite8demo

--- applications/basicSOP/src/router/index.ts
+++ applications/basicSOP/src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory, Router } from 'vue-router'
+import { createRouter, createWebHashHistory, Router } from 'vue-router'
 import { constantRoutes } from './routes'
 
 // 创建路由实例
 const router: Router = createRouter({
   // 使用 HTML5 History 模式
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   // 初始只挂载常量路由
   routes: constantRoutes,
   // 路由跳转时滚动到顶部
@@ -14,7 +14,7 @@ const router: Router = createRouter({
 // 重置路由的方法（常用于用户退出登录时清理动态路由）
 export function resetRouter() {
   const newRouter = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: constantRoutes
   })
   // 替换路由的 matcher
